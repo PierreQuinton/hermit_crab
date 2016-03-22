1e340f9104f6b5ca1194b9f90fe5142b6884a2cb
PierreQuinton <pierre.quinton@epfl.ch>
2016-03-22 14:53:35 +0100

    pushed base code for bot

--- test.py
+++ test.py
@@ -4,10 +4,10 @@ import urllib2
 import requests
 
 user='Calamar'
-passw=urllib2.quote('bot2016')
+passw=urllib2.quote('poulpe')
 baseurl='http://wikipast.world/wiki/'
 login_params='?action=login&lgname=%s&lgpassword=%s&format=json'% (user,passw)
-summary='Créatibot update'
+summary='Calamarisation'
 
 # Login request
 r1=requests.post(baseurl+'api.php'+login_params)
@@ -16,3 +16,14 @@ login_token=r1.json()['login']['token']
 #login confirm
 login_params2=login_params+'&lgtoken=%s'% login_token
 r2=requests.post(baseurl+'api.php'+login_params2,cookies=r1.cookies)
+
+#get edit token2
+params3='?format=json&action=query&meta=tokens&continue='
+r3=requests.get(baseurl+'api.php'+params3,cookies=r2.cookies)
+edit_token=r3.json()['query']['tokens']['csrftoken']
+
+edit_cookie=r2.cookies.copy()
+edit_cookie.update(r3.cookies)
+
+#actual code
+
