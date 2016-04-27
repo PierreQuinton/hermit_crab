d73f0c91cb5f83c432e96582d708a90f02c98891
PierreQuinton <pierre.quinton@epfl.ch>
2016-04-27 13:18:55 +0200

    added base for bot class

---
+++ bot.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+class Functionality:
+    """Represent a functionality of a bot"""
+    def __init__(self, content):
+        #parse 
+
+class Bot:
+    """Representaion of a Bot"""
+    def __init__(self, content):
+        """
+        :param content: content of the page (give it wiki.readpage(myPage))
+        """
+        #parse Functionalities
