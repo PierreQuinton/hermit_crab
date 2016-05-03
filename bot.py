8595030fb338c39e4cd5d8cb0e935a71695fca05
Pierre Quinton <pierre.quinton@epfl.ch>
2016-05-03 14:12:19 +0200

    blob

--- bot.py
+++ bot.py
@@ -13,6 +13,20 @@ def getSections(content):
         res.update({x.group(1):x.group(3)})
     return res
 
+def getParts(content):
+    """
+    :param content: content to get parts from
+    :return: return dict containing {tile:content}
+    """
+    pattern = r'<strong>([^<]+)</strong>\s+((([^<]+)(\s+))+)(?=(<strong>))'
+    res = {}
+    for x in re.finditer(pattern, content):
+        res.update({x.group(1).lower():x.group(2)})
+    pattern = r'<strong>([^<]+)</strong>\s+((([^<]+)(\s+))+)$'
+    m = re.search(pattern, content)
+    res.update({m.group(1).lower():m.group(2)})
+    return res
+
 class Bot:
     """Representaion of a Bot"""
     def __init__(self, content):
@@ -21,3 +35,6 @@ class Bot:
         """
         functions = getSections(content)
         print(functions)
+        parts = getParts(functions['Replace Words'])
+        print(parts)
+        
